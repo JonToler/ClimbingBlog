02b8f37cc650291d3c88838cb10e859436f9c4dd
Jon Toler and Nhat Hoang <jon@>
2016-10-26 16:02:50 -0700

    add actions call to the save button in the trail/climb_id page.

--- app/routes/trail.js
+++ app/routes/trail.js
@@ -4,4 +4,15 @@ export default Ember.Route.extend({
   model(params) {
      return this.store.findRecord('climb', params.climb_id);
    },
+   actions: {
+     update(climb, params) {
+       Object.keys(params).forEach(function(key) {
+         if(params[key]!==undefined) {
+           climb.set(key,params[key]);
+         }
+       });
+     climb.save();
+     this.transitionTo("trail");
+     }
+   }
  });
